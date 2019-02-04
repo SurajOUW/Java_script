f93f716693427808739d11626ae330f3c208211b
SurajOUW <43401886+SurajOUW@users.noreply.github.com>
2019-02-04 09:24:35 +0100

    Add files via upload

---
+++ Javascript-cours/lesson1/07.functions/index.js
@@ -0,0 +1,8 @@
+const myArr = [1, 'Hello', true];
+console.log('myArr', myArr);
+console.log(myArr[0]);
+console.log(myArr[myArr.length - 1]);
+
+const myArr = myArr;
+console.log();
+console.log();
